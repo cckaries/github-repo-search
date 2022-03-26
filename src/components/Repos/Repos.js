ab7484f860939ffb76dfbde003f86d6879a0549d
Shou-Chi Chang <cckaries@gmail.com>
2022-03-27 03:00:41 +0800

    refactor(repos): improve code

--- src/components/Repos/Repos.js
+++ src/components/Repos/Repos.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+import { useReducer, memo } from 'react';
 
 import styles from './Repos.module.scss';
 import ReposTable from './ReposTable/ReposTable';
@@ -18,143 +18,149 @@ const orderOptions = {
   asc: 'ASC',
 };
 
-const Repos = ({
-  repos = [],
-  reposCount = 0,
-  isReposReady = false,
-  prevSearch = {},
-  onGetSearchRepos = () => {},
-}) => {
-  const [{ updatedAt }, setState] = useReducer(
-    (prevState, nextState) => ({ ...prevState, ...nextState }),
-    {
-      updatedAt: null,
-    }
-  );
-
-  const debounce = (fn = () => {}, delayMs = 500) => {
-    return (...args) => {
-      if (!!timer) {
-        clearTimeout(timer);
+const Repos = memo(
+  ({
+    reposCount = 0,
+    isReposLoading = false,
+    prevSearch = {},
+    searchError = null,
+    onGetSearchRepos = () => {},
+  }) => {
+    const [{ updatedAt }, setState] = useReducer(
+      (prevState, nextState) => ({ ...prevState, ...nextState }),
+      {
+        updatedAt: null,
       }
-      timer = setTimeout(() => fn(...args), delayMs);
+    );
+
+    const debounce = (fn = () => {}, delayMs = 500) => {
+      return (...args) => {
+        if (!!timer) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(() => fn(...args), delayMs);
+      };
     };
-  };
 
-  const searchText = async q => {
-    const processedQ = q.trim().toLowerCase();
+    const searchText = async q => {
+      const processedQ = q.trim().toLowerCase();
 
-    if (processedQ !== prevSearch.q) {
-      setState({ updatedAt: Date.now() });
-      await onGetSearchRepos({ q: processedQ, page: 1 });
-    }
-  };
+      if (processedQ !== prevSearch.q) {
+        setState({ updatedAt: Date.now() });
+        await onGetSearchRepos({ q: processedQ, page: 1 });
+      }
+    };
 
-  const changeSort = (sort = null) => {
-    if (sort !== prevSearch.sort) {
-      setState({ updatedAt: Date.now() });
-      onGetSearchRepos({
-        sort,
-        order: !sort ? '' : prevSearch.order,
-        page: 1,
-      });
-    }
-  };
+    const changeSort = (sort = null) => {
+      if (sort !== prevSearch.sort) {
+        setState({ updatedAt: Date.now() });
+        onGetSearchRepos({
+          sort,
+          order: !sort ? '' : prevSearch.order,
+          page: 1,
+        });
+      }
+    };
 
-  const changeOrder = (order = null) => {
-    if (!!prevSearch.sort && order !== prevSearch.order) {
-      setState({ updatedAt: Date.now() });
-      onGetSearchRepos({ order, page: 1 });
-    }
-  };
+    const changeOrder = (order = null) => {
+      if (!!prevSearch.sort && order !== prevSearch.order) {
+        setState({ updatedAt: Date.now() });
+        onGetSearchRepos({ order, page: 1 });
+      }
+    };
 
-  const changePage = (page = null) => {
-    if (page !== prevSearch.page) {
-      onGetSearchRepos({ page });
-    }
-  };
+    const changePage = (page = null) => {
+      if (page !== prevSearch.page) {
+        onGetSearchRepos({ page });
+      }
+    };
 
-  const scrollPage = e => {
-    const { clientHeight, scrollHeight, scrollTop } = e.target;
+    const scrollPage = e => {
+      const { clientHeight, scrollHeight, scrollTop } = e.target;
 
-    if (isReposReady && clientHeight + scrollTop > scrollHeight - 5) {
-      changePage(prevSearch.page + 1);
-    }
-  };
+      if (
+        !searchError &&
+        !isReposLoading &&
+        clientHeight + scrollTop > scrollHeight - 5
+      ) {
+        changePage(prevSearch.page + 1);
+      }
+    };
 
-  const selectDom = ({
-    name = null,
-    id = null,
-    value = '',
-    placeholder = '',
-    options = {},
-    isDefaultChecked = false,
-    isDisabled = false,
-    onChange = () => {},
-  }) => (
-    <select
-      name={name}
-      id={id}
-      value={value}
-      disabled={isDisabled}
-      onChange={onChange}
-    >
-      {!isDefaultChecked && (
-        <option value="">{placeholder || 'Select...'}</option>
-      )}
-      {Object.keys(options).map((optionKey, optionIdx) => (
-        <option
-          key={optionIdx}
-          value={optionKey}
-          checked={optionKey === value}
-          defaultChecked={isDefaultChecked && !optionIdx}
-        >
-          {options[optionKey]}
-        </option>
-      ))}
-    </select>
-  );
+    const selectDom = ({
+      name = null,
+      id = null,
+      value = '',
+      placeholder = '',
+      options = {},
+      isDefaultChecked = false,
+      isDisabled = false,
+      onChange = () => {},
+    }) => (
+      <select
+        name={name}
+        id={id}
+        value={value}
+        disabled={isDisabled}
+        onChange={onChange}
+      >
+        {!isDefaultChecked && (
+          <option value="">{placeholder || 'Select...'}</option>
+        )}
+        {Object.keys(options).map((optionKey, optionIdx) => (
+          <option
+            key={optionIdx}
+            value={optionKey}
+            checked={optionKey === value}
+            defaultChecked={isDefaultChecked && !optionIdx}
+          >
+            {options[optionKey]}
+          </option>
+        ))}
+      </select>
+    );
 
-  return (
-    <div id="container" className={styles.Container}>
-      <div className={styles.Top}>
-        <div className={styles.Search}>
-          <Search
-            placeholder="Search..."
-            onInput={e => {
-              debounce(searchText)(e.target.value);
-            }}
-          />
-          {selectDom({
-            name: 'sort',
-            id: 'search-sort',
-            value: prevSearch.sort,
-            placeholder: 'Sort by...',
-            options: sortOptions,
-            onChange: e => changeSort(e.target.value),
-          })}
-          {selectDom({
-            name: 'order',
-            id: 'search-order',
-            value: prevSearch.order,
-            options: orderOptions,
-            isDefaultChecked: true,
-            isDisabled: !prevSearch.sort,
-            onChange: e => changeOrder(e.target.value),
-          })}
+    return (
+      <div id="container" className={styles.Container}>
+        <div className={styles.Top}>
+          <div className={styles.Search}>
+            <Search
+              placeholder="Search..."
+              onInput={e => {
+                debounce(searchText)(e.target.value);
+              }}
+            />
+            {selectDom({
+              name: 'sort',
+              id: 'search-sort',
+              value: prevSearch.sort,
+              placeholder: 'Sort by...',
+              options: sortOptions,
+              onChange: e => changeSort(e.target.value),
+            })}
+            {selectDom({
+              name: 'order',
+              id: 'search-order',
+              value: prevSearch.order,
+              options: orderOptions,
+              isDefaultChecked: true,
+              isDisabled: !prevSearch.sort,
+              onChange: e => changeOrder(e.target.value),
+            })}
+          </div>
+          <div className={styles.Count}>Total: {reposCount}</div>
         </div>
-        <div className={styles.Count}>Total: {reposCount}</div>
+        {!!searchError && <div className={styles.Error}>{searchError}</div>}
+        <div className={styles.Table}>
+          <ReposTable
+            updatedAt={updatedAt}
+            isLoading={isReposLoading}
+            onScroll={scrollPage}
+          />
+        </div>
       </div>
-      <div className={styles.Table}>
-        <ReposTable
-          repos={repos}
-          updatedAt={updatedAt}
-          isLoading={!isReposReady}
-          onScroll={scrollPage}
-        />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default Repos;
--- src/components/Repos/ReposTable/ReposTable.js
+++ src/components/Repos/ReposTable/ReposTable.js
@@ -1,4 +1,5 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, memo } from 'react';
+import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { format, parseISO } from 'date-fns';
 
@@ -10,67 +11,67 @@ import Tr from '../../shared/Table/Tr/Tr';
 import Th from '../../shared/Table/Th/Th';
 import Td from '../../shared/Table/Td/Td';
 
-const ReposTable = ({
-  repos = [],
-  updatedAt = null,
-  isLoading = false,
-  onScroll = () => {},
-}) => {
-  const tbodyRef = useRef(null);
+const ReposTable = memo(
+  ({ updatedAt = null, onScroll = () => {} }) => {
+    const { repos, isReposLoading } = useSelector(state => state.repos);
+    const tbodyRef = useRef(null);
 
-  useEffect(() => {
-    if (!!updatedAt) {
-      tbodyRef.current.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-  }, [updatedAt]);
+    useEffect(() => {
+      if (!!updatedAt) {
+        tbodyRef.current.scrollTo({ top: 0, behavior: 'smooth' });
+      }
+    }, [updatedAt]);
 
-  const loadingDom = (
-    <Tr customClass={styles.Loading}>
-      <td>Loading...</td>
-    </Tr>
-  );
+    const loadingDom = (
+      <Tr customClass={styles.Loading}>
+        <td>Loading...</td>
+      </Tr>
+    );
 
-  const reposDom = repos.map((repo, repoIdx) => (
-    <Tr
-      key={repoIdx}
-      isClickable
-      onClick={() => window.open(repo.html_url, '_blank')}
-    >
-      <Td customClass={styles.Cell}>{repo.full_name}</Td>
-      <Td customClass={styles.Cell}>{repo.description}</Td>
-      <Td customClass={styles.Cell}>{repo.language}</Td>
-      <Td customClass={styles.Cell}>
-        {format(parseISO(repo.updated_at), 'MMM. d, yyyy')}
-      </Td>
-      <Td customClass={styles.Cell}>{repo.stargazers_count}</Td>
-      <Td customClass={styles.Cell}>{repo.forks}</Td>
-    </Tr>
-  ));
+    const reposDom = repos.map((repo, repoIdx) => (
+      <Tr
+        key={repoIdx}
+        isClickable
+        isAnimationActive
+        onClick={() => window.open(repo.html_url, '_blank')}
+      >
+        <Td customClass={styles.Cell}>{repo.full_name}</Td>
+        <Td customClass={styles.Cell}>{repo.description}</Td>
+        <Td customClass={styles.Cell}>{repo.language}</Td>
+        <Td customClass={styles.Cell}>
+          {format(parseISO(repo.updated_at), 'MMM. d, yyyy')}
+        </Td>
+        <Td customClass={styles.Cell}>{repo.stargazers_count}</Td>
+        <Td customClass={styles.Cell}>{repo.forks}</Td>
+      </Tr>
+    ));
 
-  return (
-    <div className={styles.Container}>
-      <Table>
-        <Thead>
-          <Tr>
-            <Th customClass={styles.Cell}>Name</Th>
-            <Th customClass={styles.Cell}>Description</Th>
-            <Th customClass={styles.Cell}>Language</Th>
-            <Th customClass={styles.Cell}>Updated At</Th>
-            <Th customClass={styles.Cell}>
-              <FontAwesomeIcon icon="fa-solid fa-star" />
-            </Th>
-            <Th customClass={styles.Cell}>
-              <FontAwesomeIcon icon="fa-solid fa-code-fork" />
-            </Th>
-          </Tr>
-        </Thead>
-        <Tbody ref={tbodyRef} onScroll={onScroll}>
-          {reposDom}
-          {isLoading && loadingDom}
-        </Tbody>
-      </Table>
-    </div>
-  );
-};
+    return (
+      <div className={styles.Container}>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th customClass={styles.Cell}>Name</Th>
+              <Th customClass={styles.Cell}>Description</Th>
+              <Th customClass={styles.Cell}>Language</Th>
+              <Th customClass={styles.Cell}>Updated At</Th>
+              <Th customClass={styles.Cell}>
+                <FontAwesomeIcon icon="fa-solid fa-star" />
+              </Th>
+              <Th customClass={styles.Cell}>
+                <FontAwesomeIcon icon="fa-solid fa-code-fork" />
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody ref={tbodyRef} onScroll={onScroll}>
+            {reposDom}
+            {isReposLoading && loadingDom}
+          </Tbody>
+        </Table>
+      </div>
+    );
+  },
+  (prevProps, nextProps) => prevProps.updatedAt === nextProps.updatedAt
+);
 
 export default ReposTable;
