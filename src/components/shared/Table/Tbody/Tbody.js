8e9dd04d34a35e7b6fc4ae4e536bc5f54c2210a9
Shou-Chi Chang <cckaries@gmail.com>
2022-03-26 18:17:54 +0800

    refactor(table): improve code

--- src/components/shared/Table/Tbody/Tbody.js
+++ src/components/shared/Table/Tbody/Tbody.js
@@ -2,9 +2,15 @@ import cx from 'classnames';
 
 import styles from './Tbody.module.scss';
 
-const Tbody = ({ customClass = null, children = null }) => {
+const Tbody = ({
+  customClass = null,
+  children = null,
+  onScroll = () => {},
+}) => {
   return (
-    <tbody className={cx(styles.Container, customClass)}>{children}</tbody>
+    <tbody className={cx(styles.Container, customClass)} onScroll={onScroll}>
+      {children}
+    </tbody>
   );
 };
 
